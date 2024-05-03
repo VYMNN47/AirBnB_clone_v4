63eb52fc1b1844d8d87a8f2d2a5a6d709eb83a88
Ahmed <ahmed_l2@yahoo.com>
2024-05-03 23:04:29 +0300

    .

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,11 +1,25 @@
-document.ready(function () {
-	const amenities = {};
-	$("li input[type=checkbox]").change(function () {
-		if (this.checked) {
-			amenities[this.dataset.name] = this.dataset.id;
-		} else {
-			delete amenities[this.dataset.name];
-		}
-		$(".amenities h4").text(Object.keys(amenities).sort().join(", "));
-	});
+$(document).ready(function() {
+    const checkedAmenities = {};
+
+    $('input[type="checkbox"]').change(function() {
+        const amenityId = $(this).attr('data-id');
+        const amenityName = $(this).attr('data-name');
+
+        if ($(this).is(':checked')) {
+            checkedAmenities[amenityId] = amenityName;
+	    console.log(amenityId);
+        } else {
+            delete checkedAmenities[amenityId];
+        }
+        if (Object.keys(checkedAmenities).length === 0) {
+            $('.alist').html('&nbsp;');
+        } else {
+            updateAmenitiesList();
+	}
+    });
+
+    function updateAmenitiesList() {
+        const amenitiesList = Object.values(checkedAmenities).join(', ');
+        $('.amenities h4').text(amenitiesList);
+    }
 });
